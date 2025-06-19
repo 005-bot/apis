491d349074486207987fae2c8b635e9a1f29bd70
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-06-19 16:28:20 +0700

    [models] introduce details models

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='apis',
-    version='0.1.0',
-    description='Reusable data structures',
-    author='Aleksandr Soloshenko',
+    name="apis",
+    version="0.1.0",
+    description="Reusable data structures",
+    author="Aleksandr Soloshenko",
     packages=find_packages(),
-    install_requires=[],
-    python_requires='>=3.8'
+    install_requires=[
+        "pydantic",
+    ],
+    python_requires=">=3.8",
 )
